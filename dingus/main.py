2c58360dd4b883b8fa137b349cd06fe3de085f81
Chrysopoeia <op_adept@yahoo.co.uk>
2017-02-07 18:20:50 +0200

    blob added

---
+++ dingus/main.py
@@ -0,0 +1,57 @@
+import pygame
+
+
+def generate_map_layout(w, h):
+    return [[{}]*w for x in range(0,h)]
+
+
+class TileMap(object):
+    size = (12, 48)
+    
+    def __init__(self):
+        self.tiles = generate_map_layout(*self.size)
+
+
+class Camera(object):
+    field_of_vision = (400, 300)
+    focal_point = (0, 0)
+
+
+class SceneController(object):
+    fps = 60
+    
+    def __init__(self, viewport):
+        self.viewport = viewport
+        self.clock = pygame.time.Clock()
+        self.camera = Camera()
+        self.tilemap = TileMap()
+    
+    def handle_events(self):
+        events = pygame.event.get()
+    
+    def tick(self):
+        dt = self.clock.tick(self.fps)
+        
+        self.update(dt)
+        self.handle_events()
+        
+        pygame.display.flip()
+        
+        return self.next
+    
+    def update(self, dt):
+        pass
+    
+    @property
+    def next(self):
+        return self
+
+
+resolution = (800, 600)
+pygame.init()
+
+viewport = pygame.display.set_mode(resolution)
+scene = SceneController(viewport)
+
+while scene:
+    scene = scene.tick()
