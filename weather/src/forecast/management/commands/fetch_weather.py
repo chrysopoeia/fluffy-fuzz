cd2933140b8d7697aeee8ecdbf5b9414b5572a5d
Chrysopoeia <op_adept@yahoo.co.uk>
2017-02-16 18:39:49 +0200

    major updates

--- weather/src/forecast/management/commands/fetch_weather.py
+++ weather/src/forecast/management/commands/fetch_weather.py
@@ -4,6 +4,7 @@ import json
 import re
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 from django.core.management.base import BaseCommand
 from ...models import WeatherForecast
@@ -19,71 +20,103 @@ class News24ForecastDecoder(json.JSONDecoder):
         return super(News24ForecastDecoder, self).decode(json_string)
 
 
-class Command(BaseCommand):
-    help = "Fetches the weather forecast for the week from News24 for the given cities (77107 is Cape Town)"
-    
-    default_cities = [77107]
-    
-    def add_arguments(self, parser):
-        parser.add_argument('city_id', nargs='*', type=int)
-    
-    def handle(self, *args, **kwargs):
-        target_cities = kwargs.pop('city_id') or self.default_cities
-        
-        for target_city in target_cities:
-            try:
-                json_result = self.fetch_forecast(target_city)
-                self.import_result(json_result)
-            except Exception as e:
-                logger.exception('Could not retrieve the weather for city: %s' % target_city)
+class News24Forecast(object):
+    # map internal IDs to external ones
+    CITY_MAP = {
+        1: '77107',
+    }
     
     @classmethod
-    def import_result(cls, json_result):
-        """
-        INSERT INTO forecast_weatherforecast(date, city, min_temp, max_temp, wind, rain)
-        VALUES (), ()
-        ON CONFLICT ON CONSTRAINT <forecast_weatherforecast_datecity_constraint_name>
-        DO UPDATE 
-        min_temp=EXLUCDED.min_temp, 
-        max_temp=EXLUCDED.max_temp,
-        wind=EXLUCDED.wind,
-        rain=EXLUCDED.rain
-        """
-        
-        """
-        INSERT OR REPLACE INTO
-        forecast_weatherforecast(date, city, min_temp, max_temp, wind, rain)
-        VALUES (), (), ()
-        """
-        
-        city = int(json_result['City'])
-        
-        for forecast in json_result.get('Forecasts'):
-            min_temp = float(forecast['LowTemp'] or 0)
-            max_temp = float(forecast['HighTemp'] or 0)
-            wind = float(forecast['WindSpeed'] or 0)
-            rain = float(forecast['Rainfall'] or 0)
-            
-            obj, created = WeatherForecast.objects.update_or_create(
-                city=city,
-                date=datetime(*map(int, forecast['Date'].split(','))),
-                defaults=dict(
-                    min_temp=min_temp,
-                    max_temp=max_temp,
-                    wind=wind,
-                    rain=rain,
-                )
-            )
-    
-    @classmethod
-    def fetch_forecast(cls, target_city):
+    def fetch(cls, target_city):
         response = requests.post(
             'http://weather.news24.com/ajaxpro/TwentyFour.Weather.Web.Ajax,App_Code.ashx',
             headers={
                 'X-AjaxPro-Method': 'GetForecast7Day',
             }, 
             json={
-                'cityId': str(target_city),
+                'cityId': cls.CITY_MAP[target_city],
             })
         
         return response.json(cls=News24ForecastDecoder)
+    
+    @classmethod
+    def parse(cls, json_result):
+        parsed_results = []
+        city = int(json_result['City'])
+        
+        for forecast in json_result.get('Forecasts'):
+            defaults = {
+                'min_temp': float(forecast['LowTemp'] or 0),
+                'max_temp': float(forecast['HighTemp'] or 0),
+                'wind': float(forecast['WindSpeed'] or 0),
+                'rain': float(forecast['Rainfall'] or 0),
+                'meta': json_result,
+            }
+            
+            forecast_date = datetime(*map(int, forecast['Date'].split(','))) # parse date
+            forecast_date += relativedelta(months=1)   # add one month, JS months are 0-11
+            
+            parsed_results.append(
+                dict(defaults=defaults, date=forecast_date, city=city)
+            )
+        
+        return parsed_results
+
+
+class Command(BaseCommand):
+    help = "Fetches the weather forecast for the week for the given cities (1 is Cape Town)"
+    
+    cities = {
+        1: 'Cape Town',
+    }
+    
+    default_cities = [1]
+    default_backend = News24Forecast
+    
+    def add_arguments(self, parser):
+        parser.add_argument('city_id', nargs='*', type=int)
+        parser.add_argument('--cities', help='Print a list of usable cities', action='store_true')
+        
+    def list_cities(self):
+        return str(self.cities)
+    
+    def handle(self, *args, **kwargs):
+        if kwargs.pop('cities'):
+            return self.list_cities()
+        
+        target_cities = kwargs.pop('city_id') or self.default_cities
+        backend = self.default_backend
+        
+        for target_city in target_cities:
+            try:
+                raw_json_results = backend.fetch(target_city)
+                logger.debug('results fetched %s' % str(raw_json_results))
+                
+                parsed_results = backend.parse(raw_json_results)
+                logger.debug('results parsed %s' % str(parsed_results))
+                
+                for model_data in parsed_results:
+                    obj, created = WeatherForecast.objects.update_or_create(**model_data)
+                    logger.debug('results stored %d' % obj.pk)
+                
+            except Exception as e:
+                logger.exception('Could not retrieve the weather for city: %s' % target_city)
+        
+        """
+        PostgreSQL bulk:
+        --~-~--~-~--~-~
+        INSERT INTO forecast_weatherforecast(date, city, min_temp, max_temp, wind, rain)
+        VALUES (), (), ()
+        ON CONFLICT ON CONSTRAINT <forecast_weatherforecast_datecity_constraint_name>
+        DO UPDATE 
+        min_temp=EXLUCDED.min_temp, 
+        max_temp=EXLUCDED.max_temp,
+        wind=EXLUCDED.wind,
+        rain=EXLUCDED.rain
+        
+        SQLite bulk:
+        --~-~--~-~--~-~
+        INSERT OR REPLACE INTO
+        forecast_weatherforecast(date, city, min_temp, max_temp, wind, rain)
+        VALUES (), (), ()
+        """
--- weather/src/forecast/models.py
+++ weather/src/forecast/models.py
@@ -11,9 +11,17 @@ class WeatherForecast(models.Model):
     max_temp = models.FloatField()
     wind = models.FloatField()
     rain = models.FloatField()
+    meta = models.TextField()
     
     def __unicode__(self):
-        return "%(city)d " % ()
+        return "#%(city)d @ %(date)s %(min_temp)d-%(max_temp)d temp, %(wind)d wind, %(rain)d rain" % dict(
+            city=self.city,
+            date=self.date,
+            min_temp=self.min_temp,
+            max_temp=self.max_temp,
+            wind=self.wind,
+            rain=self.rain,
+        )
     
     class Meta:
         unique_together = ('date', 'city')
