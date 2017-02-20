8d26daa715e3f4c0f57f36fc740ad16e799db6da
Chrysopoeia <op_adept@yahoo.co.uk>
2017-02-20 13:47:02 +0200

    added asset and map

--- dingus/main.py
+++ dingus/main.py
@@ -8,10 +8,26 @@ def generate_map_layout(w, h):
 
 class TileMap(object):
     size = (12, 48)
+    tile_size = (24, 24)
     
     def __init__(self, size=None):
-        self.size = size or self.size
+        w,h = self.size = size or self.size
+        
+        tw,th = self.tile_size
+        
         self.tiles = generate_map_layout(*self.size)
+        self.viewport = pygame.Surface((w*tw,h*th))
+        
+    def render(self):
+        tw, th = self.tile_size
+        cobble = pygame.image.load('assets/6903_1.jpg')
+        
+        for row_num, row in enumerate(self.tiles):
+            for col_num, col in enumerate(row):
+                self.viewport.blit(cobble, (col_num*tw, row_num*th))
+        
+    def draw(self, surface):
+        surface.blit(self.viewport, (0,0))
 
 
 class Camera(object):
@@ -47,10 +63,20 @@ class SceneController(object):
         return self
 
 
+class GameController(SceneController):
+    def __init__(self, *args, **kwargs):
+        super(GameController, self).__init__(*args, **kwargs)
+        
+        self.map = TileMap()
+        self.map.render()
+        self.map.draw(self.viewport)
+
+
 pygame.init()
 
 viewport = pygame.display.set_mode(config.RESOLUTION)
-scene = SceneController(viewport)
+scene = GameController(viewport)
+
 
 while scene:
     scene = scene.tick()
