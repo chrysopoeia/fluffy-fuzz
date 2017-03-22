1bd151f6b4aa0924b4019d788a1ab35b83da31f2
Chrysopoeia <op_adept@yahoo.co.uk>
2017-03-22 18:14:08 +0200

    wefonwef

--- dingus/main.py
+++ dingus/main.py
@@ -7,34 +7,12 @@ RESOLUTION = (800, 600)
 
 
 class TileMap(object):
-    tile_size = (10, 10)
-    
     def __init__(self, *args, **kwargs):
-        map_size = (10, 10)
-        
-        self.tiles = self.generate_terrain(*map_size)
-        self.image = pygame.surface.Surface((100, 100))
-        
-        self.render()
+        self.tiles = self.generate_terrain(10,10)
     
     @staticmethod
     def generate_terrain(w,h):
         return [[{}]*w for x in xrange(0,h)]
-    
-    def draw(self, surface):
-        return surface.blit(self.image, (0,0))
-    
-    def render(self):
-        surface = self.image
-        tiles = self.tiles
-        tw,th = self.tile_size
-        
-        for row_num, row in enumerate(tiles):
-            for col_num, col in enumerate(row):
-                tile_color = (255, random.randint(0,255), 0)
-                tile_rect = (col_num*tw, row_num*th, tw, th)
-                
-                surface.fill(tile_color, tile_rect)
 
 
 class GameController(BaseController):
@@ -51,13 +29,11 @@ class BattleController(BaseController):
     def __init__(self, *args, **kwargs):
         super(BattleController, self).__init__(*args, **kwargs)
         
-        self.background = TileMap()
+        self.tilemap = TileMap()
         self.entities = pygame.sprite.Group()
         
     def tick(self, events, parent=None):
         super(BattleController, self).tick(events, parent=None)
-        
-        self.background.draw(self.viewport)
         
         self.entities.update()
         self.entities.draw(self.viewport)
