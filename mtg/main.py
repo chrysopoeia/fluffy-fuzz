2802a913e7648eecef45cc6ee3042ebac29cbf95
Chrysopoeia <op_adept@yahoo.co.uk>
2017-03-22 20:07:30 +0200

    thingo

--- mtg/main.py
+++ mtg/main.py
@@ -1,29 +1,68 @@
-cards = [
-    {
-        'name': 'counterspell',
-        'mana_cost': 'uu',
-        'sets': [('limited edition alpha', 'uncommon'),('limited edition beta', 'uncommon')],
-        'types': ['instant'],
-    },
-]
+db = [{
+    'name': 'counterspell',
+    'mana_cost': 'uu',
+    'sets': [('limited edition alpha', 'uncommon'),('limited edition beta', 'uncommon')],
+    'types': ['instant'],
+}]
 
 
-class Module(object):
+class Urza(object):
+    def identify(self, card_image):
+        # AI wizard thingo
+        card_details = card_image
+        
+        return card_details
+
+
+class InputTray(object):
+    def __init__(self):
+        self.physical_pile = []
+    
+    def physical_load(self, *card):
+        self.physical_pile.extend(card)
+        
+    def next(self):
+        return self.physical_pile.pop(0)
+
+
+class Scanner(object):
+    def scan(self, physical_card):
+        # scan the card face for an image
+        image = physical_card
+        
+        return image
+
+
+class Filter(object):
     pass
 
-class Filter(Module):
-    pass
 
-class Sleever(Module):
-    pass
-
-class Cleaner(Module):
-    pass
-
-
-base_filter = []
-pile = [None]*10
+class MTGO(object):
+    def lookup(self, card_details):
+        # online lookup
+        current_card = card_details
+        
+        return current_card
 
 
-while pile:
-    print pile.pop()
+urza = Urza()
+mtgo = MTGO()
+
+input_tray = InputTray()
+
+input_tray.physical_load(241,24,21,5,4,3,46,57,45)
+input_tray.physical_load(223,23421,213465,2346,43,623)
+
+scanner = Scanner()
+
+
+while input_tray.physical_pile:
+    next_card = input_tray.next()
+    
+    image = scanner.scan(next_card)
+    card_details = urza.identify(image)
+    current_card = mtgo.lookup(card_details)
+    
+    # apply filters
+    
+    print current_card
